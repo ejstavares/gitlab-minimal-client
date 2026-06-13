80313221e03484f90ceda0b637340b782fd673b3
ejstavares <ederlino.tavares@gmail.com>
2026-06-13 11:48:53 -0100

    improve multi-lang AND list commits/pipelines executions.

---
+++ i18n/languages.js
@@ -0,0 +1,6 @@
+window.GL_LANGS = [
+  { "code": "pt", "label": "PT", "name": "Português" },
+  { "code": "en", "label": "EN", "name": "English" },
+  { "code": "fr", "label": "FR", "name": "Français" },
+  { "code": "es", "label": "ES", "name": "Español" }
+];
